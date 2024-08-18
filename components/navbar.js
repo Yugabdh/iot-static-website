f686409980289aa7e10d722172dbbf96c74072f4
Yugabdh <yugabdhppashte@gmail.com>
2024-08-18 19:48:37 +0530

    small screen active link styling fixed\

--- components/navbar.js
+++ components/navbar.js
@@ -73,7 +73,7 @@ export default function NavBar() {
                                     <Link
                                         key={item.title}
                                         href={item.href}
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        className={clsx("-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50", path==item.href? "text-sky-700 bg-gray-50": "text-gray-900")}
                                     >
                                         {item.title}
                                     </Link>

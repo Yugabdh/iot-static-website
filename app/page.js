8bb162d946616e1f4b87dca42876659e0df6964d
Yugabdh <yugabdhppashte@gmail.com>
2024-08-18 18:46:35 +0530

    Removed Testimonials component completely, renamed primary import files to index.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import AboutSection from "@/components/about-section/about-section";
-import FeatureSection from "@/components/features-section/features-section";
-import HeroSection from "@/components/hero-section/hero-section";
+import AboutSection from "@/components/about-section";
+import FeatureSection from "@/components/features-section";
+import HeroSection from "@/components/hero-section";
 
 export default function Home() {
   return (

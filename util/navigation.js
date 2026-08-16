5c9651445aafa42859b363b762e3ec4de2e80552
Yugabdh <yugabdhppashte@gmail.com>
2026-08-16 13:50:32 +0530

    Navigation clean up

--- util/navigation.js
+++ util/navigation.js
@@ -79,11 +79,6 @@ const navigationConfig = [
     },
 
     {
-        title: "Insights",
-        href: "/insights",
-    },
-
-    {
         title: "Contact Us",
         href: "/contact-us",
     },

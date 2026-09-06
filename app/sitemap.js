cd9df8cfc7eb660f42afb8ae79572925c51038db
Yugabdh <yugabdhppashte@gmail.com>
2026-09-06 19:29:47 +0530

    Sitemap update

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,28 +1,72 @@
 export default function sitemap() {
+    const lastModified = new Date();
+
     return [
         {
             url: 'https://www.sdiotecs.com/',
-            lastModified: new Date('2024-08-16'),
-            changeFrequency: 'yearly',
+            lastModified,
+            changeFrequency: 'monthly',
             priority: 1,
         },
         {
             url: 'https://www.sdiotecs.com/about-us',
-            lastModified: new Date('2024-08-16'),
+            lastModified,
             changeFrequency: 'yearly',
             priority: 0.8,
         },
         {
             url: 'https://www.sdiotecs.com/contact-us',
-            lastModified: new Date('2024-08-16'),
+            lastModified,
             changeFrequency: 'yearly',
             priority: 0.5,
         },
         {
-            url: 'https://www.sdiotecs.com/our-partners',
-            lastModified: new Date('2024-10-24'),
+            url: 'https://www.sdiotecs.com/technology-partners',
+            lastModified,
             changeFrequency: 'monthly',
-            priority: 0.5,
+            priority: 0.7,
         },
-    ]
+        {
+            url: 'https://www.sdiotecs.com/services',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.9,
+        },
+        {
+            url: 'https://www.sdiotecs.com/services/bms-supply-installation-testing-commissioning',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.sdiotecs.com/services/continuous-commissioning',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.sdiotecs.com/services/data-modelling-semantic-tagging',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.sdiotecs.com/services/cloud-iot-integration',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.sdiotecs.com/services/dashboards-and-reporting',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.sdiotecs.com/services/3d-graphics-visualisation',
+            lastModified,
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+    ];
 };
